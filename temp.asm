e9c99fb2fd993da5807fcdf4eff65d69f10f84ab
laurian_li <laurian.li@mail.utoronto.ca>
2024-08-05 20:52:12 -0400

    corrected left movement collision detection

--- temp.asm
+++ temp.asm
@@ -216,7 +216,7 @@ init_shape_done:
    ###############################################################################
 keyboard:
 	li 		$v0, 32
-	li 		$a0, 200
+	li 		$a0, 600
 	syscall
 
     lw $t0, ADDR_KBRD               # $t0 = base address for keyboard
@@ -236,13 +236,49 @@ keyboard_input:                     # A key is pressed
 
 respond_to_A:
     li $t9, 0x8A8F9A       # wall
-    
+    li $t5, 0xE4DCD1       # grey color
+    li $t7, 0xC5CCD6       # white color
     lw $a1, 0($sp)
-    subi $s1, $a1, 4
-    lw $t4, 0($s1)
-    beq $t4, $t9, Auto_drop    # check left most touches the wall
-   
+    lw $s0, 0($sp)
+    lw $s4, 4($sp)
+    lw $s2, 8($sp)
+    lw $s3, 12($sp)
+    li $t2, 1
     
+check_left_obstacle:
+    beq $t2, 4, left_movement_update    
+    subi $s1, $a1, 4
+    bne $s1, $s0, check_second_left
+    j check_left_obstacle_update
+    
+check_second_left:
+    bne $s1, $s4, check_third_left
+    j check_left_obstacle_update
+    
+check_third_left:
+    bne $s1, $s2, check_fourth_left
+    j check_left_obstacle_update
+    
+check_fourth_left:
+    bne $s1, $s3, check_left_color
+    j check_left_obstacle_update
+    
+check_left_color:                    
+    lw $t4, 0($s1)
+    bne $t4, $t5, left_move_further_check
+    j check_left_obstacle_update
+    
+left_move_further_check:   
+    bne $t4, $t7, Auto_drop    # check left most touches the wall
+
+check_left_obstacle_update:
+    mul $s1, $t2, 4
+    add $a1, $sp, $s1
+    addi $t2, $t2, 1
+    lw $a1, 0($a1)
+    j check_left_obstacle   
+
+left_movement_update:    
     lw $s0, 0($sp)      # Store address at 0($sp)
     lw $t3, 0($s0)      # store color
     subi $s0, $s0, 4    # move left
